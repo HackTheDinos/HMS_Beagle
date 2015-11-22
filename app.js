993a38fb5a3c3d2ca7adc74b912b991edff2d911
sophiavanvalkenburg <sophia.vanvalkenburg@nytimes.com>
2015-11-22 00:04:53 -0500

    refactor some methods; add click events

--- app.js
+++ app.js
@@ -19,26 +19,41 @@
  DinoGame = {
            app: {
              startGame:function() {
-               //create new game
+                //create new game
                 DinoGame.model.game==new GameTemplate();
-
+                DinoGame.app.initDeck();
+                DinoGame.app.initHand();
+              },
+              initDeck:function() {
                 //create deck of random cards (40)
                 for(int i=0<i<40;i++)
                 {
-                    DinoGame.model.game.deck.push(DinoGame.app.getCardFromDeck());
-                }               
-                //deal  player  hand
+                    DinoGame.model.game.deck.push(DinoGame.model.cards.pop());
+                }
+              },
+              initHand: function(){
                 for(int i=0<i<6;i++)
                 {
-                  DinoGame.model.players.player1.push(DinoGame.model.game.deck.pop());
+                  DinoGame.app.addCardToHand(DinoGame.app.drawCard());
                 }
-                //sync with other player hand
-                  
-                //pick who's turn it is (random)
-                
-             },
-              getCardFromDeck:function() {
-                return  DinoGame.model.cards.pop();  
+              },
+              drawCard: function(){
+                DinoGame.model.game.deck.pop()
+              },
+             removeCardFromHand: function(index){
+                return DinoGame.model.hand.splice(index, 1);
+              },
+             addCardToHand: function(card){
+                  DinoGame.model.hand.push(card);
+              },
+             addCardToEvents: function(card){
+                DinoGame.model.game.events.push(card);
+              },
+             addCardToBoard: function(card, x, y){
+                DinoGame.model.game.board[x][y] = card;
+              },
+             addCardToDiscard: function(card){
+                DinoGame.model.game.discard.push(card);
               }
            },
            model:{
@@ -47,32 +62,66 @@
            },
            dom:{}
          }
-         
 
-  
+
+
+var TurnState = {
+  clicked_id: undefined,
+  clicked_type: undefined,
+  end_turn:false
+}
+
 //end game
   //count scores
   //displays who wins
   //enables start game button
   
 //draw card
-//click "deck" button
+//click "deck" button, draw card
+$("#deck").click(function(){
+  var new_card = DinoGame.app.drawCardFromDeck();
+  DinoGame.app.addCardToHand(new_card);
+});
 
 //move
-//click card + then click table
-//or click and drag to table
+//play creature card
+//remove card from board
+//play event card
+$("#hand .species .card").click(function(){
+  TurnState.clicked_id = this.id;
+  TurnState.clicked_type = "species";
+});
+
+$("#hand .event .card").click(function(){
+  if (TurnState.clicked_id !== undefined && TurnState.clicked_type == "event"){
+    var card = DinoGame.app.removeCardFromHand(TurnState.clicked_id);
+    DinoGame.app.addCardToEvents(card);
+    TurnState.end_turn = true;
+  }else{
+    TurnState.clicked_id = this.id;
+    TurnState.clicked_type = "event";
+  }
+});
+
+$("#species-board td").click(function(){
+  if (TurnState.clicked_type == "species"){
+    var card = DinoGame.app.removeCardFromHand(TurnState.clicked_id);
+    var cell_x = $(this).data("x");
+    var cell_y = $(this).data("y");
+    DinoGame.app.addCardToBoard(card, cell_x, cell_y);
+    TurnState.end_turn = true;
+  }
+});
 
 //discard
 //click card + then click discard pile
+$(".discard").click(function(){
+  if (TurnState.clicked_id !== undefined){
+    var card = DinoGame.app.removeCardFromHand(TurnState.clicked_id);
+    DinoGame.app.addCardToDiscard(card);
+  }
+});
 
-//play event card
-//click card + then click event table cell
-
-//play creature card
-//click card + then click species table cell
-
-//remove card from board
-//click card + then click discard
 
 //end game  
 //click end game button
